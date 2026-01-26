9241bbc991839745d8e0cf675a840f5a39fabc15
Navneet Singh Arora <navneetsingh790@gmail.com>
2026-01-26 21:37:54 +0100

    Adding the first pydantic model for the response

---
+++ src/constants.py
@@ -0,0 +1,41 @@
+LLM_PROVIDERS = [
+    {
+        "name": "OpenAI",
+        "description": "OpenAI is a research organization focused on developing and advancing AI technology.",
+        "url": "https://openai.com",
+        "models": ["gpt-3.5-turbo", "gpt-4", "gpt-4o", "gpt-4o-mini"],
+    },
+    {
+        "name": "Google",
+        "description": "Google is a search engine company that provides a wide range of products and services.",
+        "url": "https://google.com",
+        "models": [
+            "gemini-1.5-flash",
+            "gemini-2.0-flash-exp",
+            "gemini-2.0-flash-128k-exp",
+        ],
+    },
+    {
+        "name": "Anthropic",
+        "description": "Anthropic is a research organization focused on developing and advancing AI technology.",
+        "url": "https://anthropic.com",
+        "models": [
+            "claude-3-5-sonnet",
+            "claude-3-5-haiku",
+            "claude-3-5-sonnet-20240620",
+            "claude-3-5-haiku-20240620",
+        ],
+    },
+    {
+        "name": "Cohere",
+        "description": "Cohere is a research organization focused on developing and advancing AI technology.",
+        "url": "https://cohere.com",
+        "models": ["command-2", "command-2-pro", "command-2-pro-20240620"],
+    },
+    {
+        "name": "HuggingFace",
+        "description": "HuggingFace is a research organization focused on developing and advancing AI technology.",
+        "url": "https://huggingface.co",
+        "models": ["gpt-2", "gpt-2-medium", "gpt-2-large", "gpt-2-xl"],
+    },
+]
--- src/routers.py
+++ src/routers.py
@@ -1,18 +1,12 @@
+from models import ProviderResponse
 from fastapi import APIRouter
+from constants import LLM_PROVIDERS
 
 router = APIRouter()
 
 
-@router.get("/providers/")
-def get_providers(provider_name: str = None) -> dict | list:
-    providers = {
-        "AI Providers": ["OpenAI", "Google", "Anthropic", "Cohere", "HuggingFace"]
-    }
-    if provider_name is None:
-        return providers
-    if provider_name not in providers["AI Providers"]:
-        return {
-            "success": False,
-            "message": f"Provider {provider_name} is not available",
-        }
-    return {"success": True, "message": f"Provider {provider_name} is available"}
+@router.get("/providers/", response_model=list[ProviderResponse])
+def get_providers(company: str = None) -> list[ProviderResponse]:
+    if company:
+        return [provider for provider in LLM_PROVIDERS if provider["name"] == company]
+    return LLM_PROVIDERS
